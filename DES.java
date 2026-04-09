3bf123c8ea3f3afcad29a3ef3dbfcc74d675bfc1
liamander16 <liamander16@gmail.com>
2026-04-09 16:18:24 +1000

    Skeleton main and DES java files

---
+++ DES.java
@@ -0,0 +1,9 @@
+/// DES.java
+/// 
+/// Public Class used to contain the seperate DES functions used for encryption and decryption.
+/// 
+/// 
+
+class DES {
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+/// Main.java
+/// 
+/// Main class for the DES encryption and decryption algorithm
+///  by Liam Anderson,  3404752
+///     Guotai Xiao,    3501343 
+/// 
+
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
